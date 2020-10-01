65ee4fcaa7d48e0a6b855068e86171baa111f297
ndrju <ndrjubeatz@gmail.com>
2020-10-02 01:47:03 +0300

    feat: add min, max and avg functions for array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array === undefined) {return 0};
+    if (array.length == 0) {return 0}
+    else {
+      array.sort(function(a, b) {
+          return a - b;
+      })
+     
+    return (array[0])};
+    }
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array === undefined) {return 0};
+    if (array.length == 0) {return 0}
+    else {
+      array.sort(function(a, b) {
+          return b - a;
+      })
+     
+    return (array[0])};
+    }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined) {return 0};
+    if (array.length == 0) {return 0}
+    else {
+    return array.reduce((a, b) => a + b ) / array.length;
+    }
 }
